9d44256043c9f69cd1f8a333d69d28e1d5a04708
Trajche Petrov <contact@skechboy.com>
2014-03-29 15:53:27 +0100

    Started refactoring the project after 2y...

--- js/popup.js
+++ js/popup.js
@@ -4,100 +4,131 @@
  * This file contains Chrome Glyphs popup functions 
  *
  * @package		Chrome Glyphs
- * @category	        Popup
+ * @category	Popup
  * @author		Trajche Petrov & Aleksandar Jovanov
  * @link		https://github.com/klikstermkd/Chrome-Glyphs
 */
 
 /*
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------------------------------------------------
  * Defining main popup variables
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------------------------------------------------
 */
     var debug = true,
+        menu = document.getElementById("menu"),
         cont = document.getElementById("container"),
         copied = document.getElementById("copied"),
         clipboard = document.getElementById('clipboard'),
         dbGlyphs = JSON.parse(localStorage.getItem("glyphs"));
-
 /*
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------------------------------------------------
  * Generate glyphs
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------------------------------------------------
 */
-function generate(glyphs)
-{
-    for(cat in glyphs)
+    function generate_menu(glyphs)
     {
         var ul = document.createElement("ul");
-            ul.setAttribute('id',cat); //set id of ul element
             
-        for(item in glyphs[cat])
+        for(key in glyphs)
         {
             li = document.createElement("li");
+
+            li.data = key;
+
+            li.innerHTML = glyphs[key].name;
+                
+            li.addEventListener("click", function() // add event listener
+            {
+                generate(dbGlyphs,this.data);
+            });
+                
+            ul.appendChild(li);
+        }
+
+        menu.appendChild(ul);
+    }
+/*
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Generate glyphs
+ * ---------------------------------------------------------------------------------------------------------------------
+*/
+    function generate(glyphs,cat)
+    {
+        cont.innerHTML = '';
+        var ul = document.createElement("ul");
             
+        for(item in glyphs[cat].glyphs)
+        {
+            li = document.createElement("li");
+
             // to do disable/enable from options
-            li.setAttribute('data-tooltip',glyphs[cat][item]);
-            
-            li.innerHTML = glyphs[cat][item];
-            
+            li.setAttribute('data-tooltip',glyphs[cat].glyphs[item]);
+
+            li.innerHTML = glyphs[cat].glyphs[item];
+                
             li.addEventListener("click", function() // add event listener
             {
                 send_glyph(this.innerHTML);
             });
-            
+                
             ul.appendChild(li);          
         }
-        cont.appendChild(ul);           
+            cont.appendChild(ul);
     }
-}
-
 /*
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------------------------------------------------
  * Insert choosen glyph into selected input element or if not copy to clipboard
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------------------------------------------------
 */
-function send_glyph(glyph)
-{
-    chrome.tabs.getSelected(null, function(tab)
+    function send_glyph(glyph)
     {
-        chrome.tabs.sendRequest(tab.id, {glyph: glyph},function(response)
+        chrome.tabs.getSelected(null, function(tab)
         {
-            if(response.action == 'copy')
+            chrome.tabs.sendRequest(tab.id, {glyph: glyph},function(response)
             {
-                copy(response.glyph);
-            }
+                if(response.action == 'copy')
+                {
+                    copy(response.glyph);
+                }
+            });
         });
-    });
-}
-
-/*
- * -----------------------------------------------------------------------------
- * Copy function - copyies choosen glyph if no input element is selected
- * -----------------------------------------------------------------------------
-*/
-function copy(glyph)
-{
-    clipboard.value = glyph;					
-    clipboard.focus();
-    clipboard.select();
-    document.execCommand('Copy');
-    copied.style.display = "block";
-    timeout = setTimeout(function(){copied.style.display = "none";},3000)
-}
-
-/*
- * -----------------------------------------------------------------------------
- * Load main functions & initialize localstorage glyphs
- * -----------------------------------------------------------------------------
-*/
-(function()
-{
-    if(dbGlyphs == null || dbGlyphs === undefined || debug)
-    {
-        localStorage.setItem("glyphs", JSON.stringify(glyphs));
-	dbGlyphs = JSON.parse(localStorage.getItem("glyphs"));
     }
-    
-    generate(dbGlyphs); //generate glyphs emelements
-})();
+
+/*
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Copy function - copyies choosen glyph if no input element is selected
+ * ---------------------------------------------------------------------------------------------------------------------
+*/
+    function copy(glyph)
+    {
+        clipboard.value = glyph;					
+        clipboard.focus();
+        clipboard.select();
+        document.execCommand('Copy');
+        copied.style.height = "25px";
+        copied.style.opacity = "1";
+        timeout = setTimeout(function()
+        {
+            copied.style.height = "0px";
+            copied.style.opacity = "0";
+        },3000)
+    }
+
+/*
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Load main functions & initialize localstorage glyphs
+ * ---------------------------------------------------------------------------------------------------------------------
+*/
+    (function()
+    {
+        //console.log(dbGlyphs);
+
+        if(dbGlyphs == null || dbGlyphs === undefined || debug)
+        {
+            localStorage.setItem("glyphs", JSON.stringify(glyphs));
+            dbGlyphs = JSON.parse(localStorage.getItem("glyphs"));
+        }
+        
+        generate_menu(dbGlyphs);
+        generate(dbGlyphs,'currency'); //generate glyphs emelements
+    })();
